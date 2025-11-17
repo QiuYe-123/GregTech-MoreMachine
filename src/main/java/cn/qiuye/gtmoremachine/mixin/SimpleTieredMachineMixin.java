992fc807c6f2cdda62d5cd3b6cceafa3659ddf59
qiuye2024github <qiuye2024github@163.com>
2025-11-18 02:11:04 +0800

    fix

---
+++ src/main/java/cn/qiuye/gtmoremachine/mixin/SimpleTieredMachineMixin.java
@@ -0,0 +1,8 @@
+package cn.qiuye.gtmoremachine.mixin;
+
+import com.gregtechceu.gtceu.api.machine.SimpleTieredMachine;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(SimpleTieredMachine.class)
+public class SimpleTieredMachineMixin {
+}
