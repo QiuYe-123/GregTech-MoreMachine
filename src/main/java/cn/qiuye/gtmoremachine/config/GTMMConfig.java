28fc62da19e96958a36e99ec115b678550db3148
qiuye2024github <qiuye2024github@163.com>
2025-11-18 20:31:54 +0800

    添加配置文件类

---
+++ src/main/java/cn/qiuye/gtmoremachine/config/GTMMConfig.java
@@ -0,0 +1,26 @@
+package cn.qiuye.gtmoremachine.config;
+
+import cn.qiuye.gtmoremachine.GTmm;
+import dev.toma.configuration.Configuration;
+import dev.toma.configuration.config.Config;
+import dev.toma.configuration.config.Configurable;
+import dev.toma.configuration.config.format.ConfigFormats;
+
+@Config(id = GTmm.MOD_ID)
+public class GTMMConfig {
+
+    public static GTMMConfig INSTANCE;
+    private static final Object LOCK = new Object();
+
+    public static void init() {
+        synchronized (LOCK) {
+            if (INSTANCE == null) {
+                INSTANCE = Configuration.registerConfig(GTMMConfig.class, ConfigFormats.YAML).getConfigInstance();
+            }
+        }
+    }
+
+    @Configurable
+    @Configurable.Comment({ " " })
+    public boolean isWirelessRateEnable = true;
+}
