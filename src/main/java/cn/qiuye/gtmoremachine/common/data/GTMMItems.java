29a6ed4f32c9621c4766bedd4fede6ce557371d9
QiuYe <qiuye2024github@163.com>
2025-11-19 23:10:37 +0800

    添加无线能源链接工具 (#13)

--- src/main/java/cn/qiuye/gtmoremachine/common/data/GTMMItems.java
+++ src/main/java/cn/qiuye/gtmoremachine/common/data/GTMMItems.java
@@ -1,6 +1,32 @@
 package cn.qiuye.gtmoremachine.common.data;
 
+import cn.qiuye.gtmoremachine.GTmm;
+import cn.qiuye.gtmoremachine.common.item.WirelessEnergyBindingToolBehavior;
+
+import com.gregtechceu.gtceu.api.item.ComponentItem;
+import com.gregtechceu.gtceu.api.item.component.ICustomRenderer;
+
+import com.tterrag.registrate.util.entry.ItemEntry;
+import com.tterrag.registrate.util.nullness.NonNullConsumer;
+
+import static cn.qiuye.gtmoremachine.common.registry.GTMMRegistration.GTMMREGISTRATE;
+import static com.gregtechceu.gtceu.common.data.GTItems.attach;
+
 public class GTMMItems {
+
+    static {
+        GTMMREGISTRATE.creativeModeTab(() -> GTMMCreativeModeTabs.WIRELESS_TAB);
+    }
+
+    public static <T extends ComponentItem> NonNullConsumer<T> attachRenderer(ICustomRenderer customRenderer) {
+        return !GTmm.isClientSide() ? NonNullConsumer.noop() : (item) -> item.attachComponents(customRenderer);
+    }
+
+    public static ItemEntry<ComponentItem> WIRELESS_ENERGY_BINDING_TOOL = GTMMREGISTRATE
+            .item("wireless_energy_binding_tool", ComponentItem::create)
+            .properties(p -> p.stacksTo(1))
+            .onRegister(attach(WirelessEnergyBindingToolBehavior.Companion))
+            .register();
 
     public static void init() {}
 }
