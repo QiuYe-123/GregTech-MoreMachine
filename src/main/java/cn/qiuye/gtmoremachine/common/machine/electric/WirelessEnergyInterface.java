395a6ed77eae986cce02f8bb2e350502adac1959
qiuye2024github <qiuye2024github@163.com>
2025-11-21 16:45:50 +0800

    添加无线接口

---
+++ src/main/java/cn/qiuye/gtmoremachine/common/machine/electric/WirelessEnergyInterface.java
@@ -0,0 +1,171 @@
+package cn.qiuye.gtmoremachine.common.machine.electric;
+
+import cn.qiuye.gtmoremachine.api.machine.IWirelessEnergyContainerHolder;
+import cn.qiuye.gtmoremachine.api.misc.WirelessEnergyContainer;
+import cn.qiuye.gtmoremachine.utils.TeamUtils;
+
+import com.gregtechceu.gtceu.api.GTValues;
+import com.gregtechceu.gtceu.api.capability.recipe.IO;
+import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.api.machine.MetaMachine;
+import com.gregtechceu.gtceu.api.machine.TickableSubscription;
+import com.gregtechceu.gtceu.api.machine.feature.IInteractedMachine;
+import com.gregtechceu.gtceu.api.machine.feature.IMachineLife;
+import com.gregtechceu.gtceu.api.machine.multiblock.part.TieredIOPartMachine;
+import com.gregtechceu.gtceu.api.machine.trait.NotifiableEnergyContainer;
+import com.gregtechceu.gtceu.common.data.GTItems;
+
+import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
+import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+public class WirelessEnergyInterface extends TieredIOPartMachine implements IInteractedMachine, IMachineLife, IWirelessEnergyContainerHolder {
+
+    protected static final ManagedFieldHolder MANAGED_FIELD_HOLDER = new ManagedFieldHolder(WirelessEnergyInterface.class,
+            MetaMachine.MANAGED_FIELD_HOLDER);
+
+    public ManagedFieldHolder getFieldHolder() {
+        return MANAGED_FIELD_HOLDER;
+    }
+
+    private TickableSubscription updEnergySubs;
+
+    @Getter
+    @Setter
+    @Nullable
+    private WirelessEnergyContainer WirelessEnergyContainerCache;
+
+    @Persisted
+    public final NotifiableEnergyContainer energyContainer;
+
+    public WirelessEnergyInterface(IMachineBlockEntity holder) {
+        super(holder, GTValues.MAX, IO.IN);
+        this.energyContainer = createEnergyContainer();
+    }
+
+    protected NotifiableEnergyContainer createEnergyContainer() {
+        NotifiableEnergyContainer container;
+
+        container = NotifiableEnergyContainer.receiverContainer(this, Long.MAX_VALUE,
+                GTValues.VEX[tier], 67108864);
+        container.setSideInputCondition(s -> s == getFrontFacing() && isWorkingEnabled());
+        container.setCapabilityValidator(s -> s == null || s == getFrontFacing());
+
+        return container;
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        updateEnergySubscription();
+    }
+
+    @Override
+    public void onUnload() {
+        super.onUnload();
+        if (updEnergySubs != null) {
+            updEnergySubs.unsubscribe();
+            updEnergySubs = null;
+        }
+    }
+
+    private void updateEnergySubscription() {
+        if (this.getUUID() != null) {
+            updEnergySubs = subscribeServerTick(updEnergySubs, this::updateEnergy);
+        } else if (updEnergySubs != null) {
+            updEnergySubs.unsubscribe();
+            updEnergySubs = null;
+        }
+    }
+
+    private void updateEnergy() {
+        var currentStored = energyContainer.getEnergyStored();
+        if (currentStored <= 0) return;
+        WirelessEnergyContainer container = getWirelessEnergyContainer();
+        if (container == null) return;
+        long changeEnergy = container.addEnergy(currentStored, this);
+        if (changeEnergy > 0) energyContainer.setEnergyStored(currentStored - changeEnergy);
+    }
+
+    @Override
+    public boolean shouldOpenUI(Player player, InteractionHand hand, BlockHitResult hit) {
+        return false;
+    }
+
+    @Override
+    public InteractionResult onUse(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        if (isRemote()) return InteractionResult.PASS;
+        ItemStack is = player.getItemInHand(hand);
+        if (is.isEmpty()) return InteractionResult.PASS;
+        if (is.is(GTItems.TOOL_DATA_STICK.asItem())) {
+            setOwnerUUID(player.getUUID());
+            setWirelessEnergyContainerCache(null);
+            player.sendSystemMessage(Component.translatable("gtmoremachine.machine.wireless_energy_hatch.tooltip.bind", TeamUtils.getName(player)));
+            updateEnergySubscription();
+            return InteractionResult.SUCCESS;
+        }
+        return InteractionResult.PASS;
+    }
+
+    @Override
+    public boolean onLeftClick(Player player, Level world, InteractionHand hand, BlockPos pos, Direction direction) {
+        if (isRemote()) return false;
+        ItemStack is = player.getItemInHand(hand);
+        if (is.isEmpty()) return false;
+        if (is.is(GTItems.TOOL_DATA_STICK.asItem())) {
+            setOwnerUUID(null);
+            setWirelessEnergyContainerCache(null);
+            player.sendSystemMessage(Component.translatable("gtmthings.machine.wireless_energy_hatch.tooltip.unbind"));
+            updateEnergySubscription();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onMachinePlaced(@Nullable LivingEntity player, ItemStack stack) {
+        if (player != null) {
+            setOwnerUUID(player.getUUID());
+            updateEnergySubscription();
+        }
+    }
+
+    @Override
+    public @Nullable UUID getUUID() {
+        return this.getOwnerUUID();
+    }
+
+    //////////////////////////////////////
+    // ********** Misc **********//
+    //////////////////////////////////////
+
+    @Override
+    public int tintColor(int index) {
+        if (index == 2) {
+            return GTValues.VC[getTier()];
+        }
+        return super.tintColor(index);
+    }
+}
