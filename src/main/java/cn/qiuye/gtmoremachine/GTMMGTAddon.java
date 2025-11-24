14116abbf3b19b83f724352c69d9715abc0eefae
qiuye2024github <qiuye2024github@163.com>
2025-11-24 23:57:57 +0800

    Revert "添加配方(GTMThings)，未进行整理"
    This reverts commit cabb30719e6103f2d90b4333d5a0bd5026e6d55d.

--- src/main/java/cn/qiuye/gtmoremachine/GTMMGTAddon.java
+++ src/main/java/cn/qiuye/gtmoremachine/GTMMGTAddon.java
@@ -4,15 +4,10 @@ import cn.qiuye.gtmoremachine.common.data.GTMMBlocks;
 import cn.qiuye.gtmoremachine.common.data.GTMMItems;
 import cn.qiuye.gtmoremachine.common.registry.GTMMRegistration;
 import cn.qiuye.gtmoremachine.data.GTMMDatagen;
-import cn.qiuye.gtmoremachine.data.recipe.GTMMRecipes;
 
 import com.gregtechceu.gtceu.api.addon.GTAddon;
 import com.gregtechceu.gtceu.api.addon.IGTAddon;
 import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
-
-import net.minecraft.data.recipes.FinishedRecipe;
-
-import java.util.function.Consumer;
 
 @GTAddon
 public class GTMMGTAddon implements IGTAddon {
@@ -32,10 +27,5 @@ public class GTMMGTAddon implements IGTAddon {
         GTMMItems.init();
         GTMMBlocks.init();
         GTMMDatagen.initPost();
-    }
-
-    @Override
-    public void addRecipes(Consumer<FinishedRecipe> provider) {
-        GTMMRecipes.init(provider);
     }
 }
