6f5cffdf4c5965bb0474a3a44ea532e0e88a93af
qiuye2024github <qiuye2024github@163.com>
2025-12-12 13:10:12 +0800

    重构blockmap

--- src/main/java/cn/qiuye/gtmoremachine/common/block/BlockMap.kt
+++ src/main/java/cn/qiuye/gtmoremachine/common/block/BlockMap.kt
@@ -1,66 +1,100 @@
-package cn.qiuye.gtmoremachine.common.block;
+package cn.qiuye.gtmoremachine.common.block
 
-import cn.qiuye.gtmoremachine.api.GTMMAPI;
-import cn.qiuye.gtmoremachine.config.GTMMConfig;
+import cn.qiuye.gtmoremachine.api.GTMMAPI
+import cn.qiuye.gtmoremachine.config.GTMMConfig
 
-import com.gregtechceu.gtceu.api.GTCEuAPI;
-import com.gregtechceu.gtceu.common.data.GTBlocks;
+import com.gregtechceu.gtceu.api.GTCEuAPI
+import com.gregtechceu.gtceu.api.block.IMachineBlock
+import com.gregtechceu.gtceu.common.data.GTBlocks
 
-import net.minecraft.world.level.block.Block;
-import net.minecraftforge.common.util.Lazy;
+import net.minecraft.world.level.block.Block
+import net.minecraftforge.common.util.Lazy
 
-import it.unimi.dsi.fastutil.ints.*;
-import it.unimi.dsi.fastutil.objects.*;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap
 
-import java.math.BigInteger;
-import java.util.*;
-import java.util.function.Supplier;
+import java.util.function.Supplier
 
-public interface BlockMap {
+object BlockMap {
+    @JvmField
+    val tierBlockMap = Object2ObjectOpenHashMap<String, Lazy<Array<Block>>>(50)
 
-    Object2ObjectOpenHashMap<String, Lazy<Block[]>> tierBlockMap = new Object2ObjectOpenHashMap<>(50);
+    @JvmField
+    val rotMap = Int2ObjectOpenHashMap<Supplier<IMachineBlock>>(11)
 
-    Int2ObjectOpenHashMap<Supplier<?>> rotMap = new Int2ObjectOpenHashMap<>(11);
+    const val COIL = "item.gtmoremachine.advanced_terminal.setting.coil"
+    const val COMP = "item.gtmoremachine.advanced_terminal.setting.comp"
+    const val CLEA = "item.gtmoremachine.advanced_terminal.setting.clea"
+    const val LAMP = "item.gtmoremachine.advanced_terminal.setting.lamp"
+    const val BORLAMP = "item.gtmoremachine.advanced_terminal.setting.borlamp"
+    const val ROTOR = "item.gtmoremachine.advanced_terminal.setting.rotor"
+    const val WECC = "item.gtmoremachine.advanced_terminal.setting.wecc"
+    const val ECU = "item.gtmoremachine.advanced_terminal.setting.ecu"
 
-    String COIL = "item.gtmoremachine.advanced_terminal.setting.coil";
-    String COMP = "item.gtmoremachine.advanced_terminal.setting.comp";
-    String CLEA = "item.gtmoremachine.advanced_terminal.setting.clea";
-    String LAMP = "item.gtmoremachine.advanced_terminal.setting.lamp";
-    String BORLAMP = "item.gtmoremachine.advanced_terminal.setting.borlamp";
-    String ROTOR = "item.gtmoremachine.advanced_terminal.setting.rotor";
-    String WECC = "item.gtmoremachine.advanced_terminal.setting.wecc";
-    String ECU = "item.gtmoremachine.advanced_terminal.setting.ecu";
+    @JvmStatic
+    fun init() {
+        tierBlockMap[COIL] = Lazy.of {
+            GTCEuAPI.HEATING_COILS.entries
+                .sortedBy { it.key.coilTemperature }
+                .map { it.value.get() }
+                .toTypedArray()
+        }
 
-    static void init() {
-        tierBlockMap.put(COIL, Lazy.of(() -> GTCEuAPI.HEATING_COILS.entrySet().stream()
-                .sorted(Comparator.comparingInt(e -> e.getKey().getCoilTemperature()))
-                .map(Map.Entry::getValue).map(Supplier::get).toArray(Block[]::new)));
-        tierBlockMap.put(COMP, Lazy.of(() -> GTCEuAPI.PSS_BATTERIES.entrySet().stream()
-                .sorted(Comparator.comparingInt(e -> e.getKey().getTier()))
-                .map(Map.Entry::getValue).map(Supplier::get).toArray(Block[]::new)));
-        tierBlockMap.put(CLEA, Lazy.of(() -> GTCEuAPI.CLEANROOM_FILTERS.entrySet().stream()
-                .sorted(Comparator.comparingInt(e -> e.getKey().getCleanroomType().hashCode()))
-                .map(Map.Entry::getValue).map(Supplier::get).toArray(Block[]::new)));
-        tierBlockMap.put(LAMP, Lazy.of(() -> GTBlocks.LAMPS.entrySet().stream()
-                .sorted(Comparator.comparingInt(e -> e.getKey().getFireworkColor()))
-                .map(Map.Entry::getValue).map(Supplier::get).toArray(Block[]::new)));
-        tierBlockMap.put(BORLAMP, Lazy.of(() -> GTBlocks.BORDERLESS_LAMPS.entrySet().stream()
-                .sorted(Comparator.comparingInt(e -> e.getKey().getFireworkColor()))
-                .map(Map.Entry::getValue).map(Supplier::get).toArray(Block[]::new)));
-        tierBlockMap.put(ECU, Lazy.of(() -> GTMMAPI.ECU.entrySet().stream()
-                .sorted(Comparator.comparingInt(Map.Entry::getKey))
-                .map(Map.Entry::getValue).map(Supplier::get).toArray(Block[]::new)));
-        tierBlockMap.put(ROTOR, Lazy.of(() -> rotMap.int2ObjectEntrySet().stream()
-                .sorted(Comparator.comparingInt(Int2ObjectMap.Entry::getIntKey))
-                .map(Int2ObjectMap.Entry::getValue).map(Supplier::get).toArray(Block[]::new)));
-        if (GTMMConfig.getINSTANCE().isWirelessCapacitylimitEnable) {
-            tierBlockMap.put(WECC, Lazy.of(() -> GTMMAPI.WECC.entrySet().stream()
-                    .sorted((entry1, entry2) -> {
-                        BigInteger throughput1 = entry1.getValue().get().getData().getCapacity();
-                        BigInteger throughput2 = entry2.getValue().get().getData().getCapacity();
-                        return throughput1.compareTo(throughput2);
-                    })
-                    .map(Map.Entry::getValue).map(Supplier::get).toArray(Block[]::new)));
+        tierBlockMap[COMP] = Lazy.of {
+            GTCEuAPI.PSS_BATTERIES.entries
+                .sortedBy { it.key.tier }
+                .map { it.value.get() }
+                .toTypedArray()
+        }
+
+        tierBlockMap[CLEA] = Lazy.of {
+            GTCEuAPI.CLEANROOM_FILTERS.entries
+                .sortedBy { it.key.cleanroomType.hashCode() }
+                .map { it.value.get() }
+                .toTypedArray()
+        }
+
+        tierBlockMap[LAMP] = Lazy.of {
+            GTBlocks.LAMPS.entries
+                .sortedBy { it.key.fireworkColor }
+                .map { it.value.get() }
+                .toTypedArray()
+        }
+
+        tierBlockMap[BORLAMP] = Lazy.of {
+            GTBlocks.BORDERLESS_LAMPS.entries
+                .sortedBy { it.key.fireworkColor }
+                .map { it.value.get() }
+                .toTypedArray()
+        }
+
+        tierBlockMap[ECU] = Lazy.of {
+            GTMMAPI.ECU.entries
+                .sortedBy { it.key }
+                .map { it.value.get() }
+                .toTypedArray()
+        }
+
+        tierBlockMap[ROTOR] = Lazy.of {
+            rotMap.int2ObjectEntrySet()
+                .sortedBy { it.intKey }
+                .map { it.value.get().self() }
+                .toTypedArray()
+        }
+
+        if (GTMMConfig.INSTANCE.isWirelessCapacitylimitEnable) {
+            tierBlockMap[WECC] = Lazy.of {
+                GTMMAPI.WECC.entries
+                    .sortedWith(
+                        Comparator { entry1, entry2 ->
+                            val throughput1 = entry1.value.get().data.capacity
+                            val throughput2 = entry2.value.get().data.capacity
+                            throughput1.compareTo(throughput2)
+                        },
+                    )
+                    .map { it.value.get() }
+                    .toTypedArray()
+            }
         }
     }
 }
