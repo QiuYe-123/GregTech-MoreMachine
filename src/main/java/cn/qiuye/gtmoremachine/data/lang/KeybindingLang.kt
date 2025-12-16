9481c0a5f02c485a74d560afffc5cfdad61fb317
qiuye2024github <qiuye2024github@163.com>
2025-12-16 17:45:48 +0800

    Fix

--- src/main/java/cn/qiuye/gtmoremachine/data/lang/KeybindingLang.kt
+++ src/main/java/cn/qiuye/gtmoremachine/data/lang/KeybindingLang.kt
@@ -3,14 +3,5 @@ package cn.qiuye.gtmoremachine.data.lang
 import com.tterrag.registrate.providers.RegistrateLangProvider
 
 object KeybindingLang {
-    fun init(provider: RegistrateLangProvider) {
-        provider.add(
-            "key.gtmoremachine.category",
-            "GT More Machine",
-        )
-        provider.add(
-            "key.gtmoremachine.bind.wet",
-            "Wireless Energy Terminal",
-        )
-    }
+    fun init(provider: RegistrateLangProvider) {}
 }
--- src/main/java/cn/qiuye/gtmoremachine/utils/input/SyncedKeyMappingEvent.kt
+++
@@ -1,8 +0,0 @@
-package cn.qiuye.gtmoremachine.utils.input
-
-import net.minecraftforge.eventbus.api.Event
-import net.minecraftforge.fml.event.IModBusEvent
-
-class SyncedKeyMappingEvent :
-    Event(),
-    IModBusEvent
