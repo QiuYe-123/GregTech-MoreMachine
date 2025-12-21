3299521e62f4abaa1d9589e45a32357c42b1ae41
qiuye2024github <qiuye2024github@163.com>
2025-12-21 20:48:49 +0800

    Fixs

--- src/main/java/cn/qiuye/gtmoremachine/integration/ae/machine/multiblock/electric/MESynthesisDistributionCenter.kt
+++ src/main/java/cn/qiuye/gtmoremachine/integration/ae/machine/multiblock/electric/MESynthesisDistributionCenter.kt
@@ -1,3 +1,74 @@
-package cn.qiuye.gtmoremachine.integration.ae.machine.multiblock.electric;
+package cn.qiuye.gtmoremachine.integration.ae.machine.multiblock.electric
 
-public class MESynthesisDistributionCenter {}
+import com.gregtechceu.gtceu.api.gui.GuiTextures
+import com.gregtechceu.gtceu.api.gui.fancy.FancyMachineUIWidget
+import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity
+import com.gregtechceu.gtceu.api.machine.feature.IFancyUIMachine
+import com.gregtechceu.gtceu.api.machine.feature.multiblock.IDisplayUIMachine
+import com.gregtechceu.gtceu.api.machine.multiblock.WorkableMultiblockMachine
+
+import com.lowdragmc.lowdraglib.gui.modular.ModularUI
+import com.lowdragmc.lowdraglib.gui.util.ClickData
+import com.lowdragmc.lowdraglib.gui.widget.ComponentPanelWidget
+import com.lowdragmc.lowdraglib.gui.widget.DraggableScrollableWidgetGroup
+import com.lowdragmc.lowdraglib.gui.widget.LabelWidget
+import com.lowdragmc.lowdraglib.gui.widget.Widget
+import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup
+import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder
+
+import net.minecraft.network.chat.Component
+import net.minecraft.world.entity.player.Player
+
+open class MESynthesisDistributionCenter(holder: IMachineBlockEntity) :
+    WorkableMultiblockMachine(holder),
+    IFancyUIMachine,
+    IDisplayUIMachine {
+
+    companion object {
+        protected val MANAGED_FIELD_HOLDER: ManagedFieldHolder = ManagedFieldHolder(
+            MESynthesisDistributionCenter::class.java,
+            WorkableMultiblockMachine.MANAGED_FIELD_HOLDER,
+        )
+    }
+
+    override fun getFieldHolder(): ManagedFieldHolder = MANAGED_FIELD_HOLDER
+    // ================= UI相关方法 =================
+    /**
+     * 创建UI部件（IDisplayUIMachine接口实现）
+     * @return 配置好的UI部件组
+     */
+    override fun createUIWidget(): Widget {
+        val group = WidgetGroup(0, 0, 182 + 8, 117 + 8)
+        group.addWidget(
+            DraggableScrollableWidgetGroup(4, 4, 182, 117).setBackground(screenTexture)
+                .addWidget(LabelWidget(4, 5, self().blockState.block.descriptionId))
+                .addWidget(
+                    ComponentPanelWidget(
+                        4,
+                        17,
+                    ) { textList: MutableList<Component> -> this.addDisplayText(textList) }
+                        .setMaxWidthLimit(150)
+                        .clickHandler { componentData: String?, clickData: ClickData? ->
+                            this.handleDisplayClick(
+                                componentData,
+                                clickData,
+                            )
+                        },
+                ),
+        )
+        group.setBackground(GuiTextures.BACKGROUND_INVERSE)
+        return group
+    }
+
+    /**
+     * 创建花式UI（IFancyUIMachine接口实现）
+     */
+    override fun createUI(entityPlayer: Player): ModularUI =
+        ModularUI(198, 208, this, entityPlayer).widget(FancyMachineUIWidget(this, 198, 208))
+
+    /**
+     * 检查是否为远程端（客户端）
+     * @return 如果是客户端返回true，服务器端返回false
+     */
+    override fun isRemote(): Boolean = super<WorkableMultiblockMachine>.isRemote
+}
