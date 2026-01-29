96973cfa2e2a7588c043c0b1123f21905c393341
qiuye2024github <qiuye2024github@163.com>
2026-01-29 12:33:53 +0800

    GTMMDatagen fix

--- src/main/java/cn/qiuye/gtmoremachine/data/GTMMDatagen.kt
+++ src/main/java/cn/qiuye/gtmoremachine/data/GTMMDatagen.kt
@@ -1,19 +1,25 @@
 package cn.qiuye.gtmoremachine.data
 
+import cn.qiuye.gtmoremachine.api.lang.SimplifiedChineseLanguageProvider
+import cn.qiuye.gtmoremachine.api.lang.TraditionalChineseLanguageProvider
 import cn.qiuye.gtmoremachine.common.registry.GTMMRegistration
 import cn.qiuye.gtmoremachine.data.lang.LangHandler
 
 import com.tterrag.registrate.providers.ProviderType
-import com.tterrag.registrate.providers.RegistrateLangProvider
-import com.tterrag.registrate.util.nullness.NonNullConsumer
 
 object GTMMDatagen {
 
     @JvmStatic
     fun initPost() {
+        GTMMRegistration.GTMMREGISTRATE.addDataGenerator(ProviderType.LANG, LangHandler::init)
+        GTMMRegistration.GTMMREGISTRATE.addDataGenerator(ProviderType.LANG, LangHandler::enInitialize)
         GTMMRegistration.GTMMREGISTRATE.addDataGenerator(
-            ProviderType.LANG,
-            NonNullConsumer { provider: RegistrateLangProvider -> LangHandler.init(provider) },
+            SimplifiedChineseLanguageProvider.LANG,
+            LangHandler::cnInitialize,
+        )
+        GTMMRegistration.GTMMREGISTRATE.addDataGenerator(
+            TraditionalChineseLanguageProvider.LANG,
+            LangHandler::twInitialize,
         )
     }
 }
