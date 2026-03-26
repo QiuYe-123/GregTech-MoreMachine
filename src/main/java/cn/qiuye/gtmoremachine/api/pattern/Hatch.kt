03d85afde25dd40dd3e89fe89865292390a77c6b
qiuye2024github <qiuye2024github@163.com>
2026-03-26 12:41:27 +0800

    spotless

--- src/main/java/cn/qiuye/gtmoremachine/api/pattern/Hatch.kt
+++ src/main/java/cn/qiuye/gtmoremachine/api/pattern/Hatch.kt
@@ -2,10 +2,11 @@ package cn.qiuye.gtmoremachine.api.pattern
 
 import com.gregtechceu.gtceu.api.machine.multiblock.part.MultiblockPartMachine
 import com.gregtechceu.gtceu.api.registry.GTRegistries
-import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet
 
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.block.Block
+
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet
 
 object Hatch {
 
