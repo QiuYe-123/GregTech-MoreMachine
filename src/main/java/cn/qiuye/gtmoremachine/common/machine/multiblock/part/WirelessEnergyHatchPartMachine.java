0f93d83aca56e6ecb2932dc547e553d123a32d07
qiuye2024github <qiuye2024github@163.com>
2026-04-23 21:23:29 +0800

    fix

--- src/main/java/cn/qiuye/gtmoremachine/common/machine/multiblock/part/WirelessEnergyHatchPartMachine.java
+++ src/main/java/cn/qiuye/gtmoremachine/common/machine/multiblock/part/WirelessEnergyHatchPartMachine.java
@@ -20,7 +20,6 @@ import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -159,14 +158,6 @@ public class WirelessEnergyHatchPartMachine extends TieredIOPartMachine implemen
             return true;
         }
         return false;
-    }
-
-    @Override
-    public void onMachinePlaced(@Nullable LivingEntity player, ItemStack stack) {
-        if (player != null) {
-            setOwnerUUID(player.getUUID());
-            updateEnergySubscription();
-        }
     }
 
     @Override
